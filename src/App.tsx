2390318e2c9c2adcdf26b3b73ec9f7719eaac466
LYH <2441697223@qq.com>
2024-10-27 17:37:41 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 'jiaofae';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit really???<code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
